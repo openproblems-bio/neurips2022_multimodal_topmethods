047f8b09a752ec2c4725ae963e3e0a8b9d99db97
Robrecht Cannoodt <rcannood@gmail.com>
2023-04-25 10:29:23 +0200

    update scripts and readmes

--- src/shuji_suzuki/description.md
+++ src/shuji_suzuki/description.md
@@ -92,3 +92,35 @@ I used the weighted average of predictions of the following models.
       4, 7 only, etc.
     - Use a model trained on the full training data set as a
       pre-training model
+
+## Development setup
+
+Download resources
+
+``` bash
+res_dir=src/shuji_suzuki/resources
+mkdir -p "$res_dir"
+wget https://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/tsv/hgnc_complete_set.txt -O "$res_dir/hgnc_complete_set.txt"
+wget https://reactome.org/download/current/ReactomePathways.gmt.zip -O "$res_dir/ReactomePathways.gmt.zip" &&
+  unzip "$res_dir/ReactomePathways.gmt.zip" -d "$res_dir" && 
+  rm "$res_dir/ReactomePathways.gmt.zip"
+```
+
+Clone repo
+
+``` bash
+echo shu65_openproblems > src/shuji_suzuki/.gitignore
+git clone https://github.com/shu65/open-problems-multimodal.git src/shuji_suzuki/shu65_openproblems
+```
+
+## Executing the method
+
+Run method
+
+``` bash
+viash run src/shuji_suzuki/config.vsh.yaml -- \
+  --input sample_data \
+  --output output \
+  ---memory 100GB \
+  ---cpus 30
+```
--- src/shuji_suzuki/script.sh
+++ src/shuji_suzuki/script.sh
@@ -1,14 +1,28 @@
 #/bin/bash
 
-shu65_openproblems=/opt/shu65_openproblems
+repo=/opt/shu65_openproblems
 
 ## VIASH START
 meta_resources_dir=src/shuji_suzuki
-shu65_openproblems=src/shuji_suzuki/shu65_openproblems
+repo=src/shuji_suzuki/shu65_openproblems
 ## VIASH END
 
-python3 script/make_compressed_dataset.py --data_dir "$par_input"
-python3 script/make_additional_files.py --data_dir "$par_input"
-python3 script/make_cite_input_mask.py --data_dir "$par_input" \
+echo "Copying data to output"
+cp -r "$par_input" "$par_output"
+
+echo "Making compressed dataset"
+python3 $repo/script/make_compressed_dataset.py --data_dir "$par_output"
+
+echo "Making additional files"
+python3 $repo/script/make_additional_files.py --data_dir "$par_output"
+
+echo "Making cite input mask"
+python3 $repo/script/make_cite_input_mask.py --data_dir "$par_output" \
   --hgnc_complete_set_path "$meta_resources_dir/hgnc_complete_set.txt" \
   --reactome_pathways_path "$meta_resources_dir/ReactomePathways.gmt"
+
+echo "Training multi"
+python3 scripts/train_mode.py --data_dir "$par_output" --task_type multi 
+
+echo "Training cite"
+python3 scripts/train_mode.py --data_dir "$par_output" --task_type cite 
