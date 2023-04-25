90d24bae74f071880ddefd7c9a6b146f0c8d7d55
Robrecht Cannoodt <rcannood@gmail.com>
2023-04-25 11:10:52 +0200

    fix scripts

--- src/shuji_suzuki/script.sh
+++ src/shuji_suzuki/script.sh
@@ -11,18 +11,18 @@ echo "Copying data to output"
 cp -r "$par_input" "$par_output"
 
 echo "Making compressed dataset"
-python3 $repo/script/make_compressed_dataset.py --data_dir "$par_output"
+python3 "$repo/script/make_compressed_dataset.py" --data_dir "$par_output"
 
 echo "Making additional files"
-python3 $repo/script/make_additional_files.py --data_dir "$par_output"
+python3 "$repo/script/make_additional_files.py" --data_dir "$par_output"
 
 echo "Making cite input mask"
-python3 $repo/script/make_cite_input_mask.py --data_dir "$par_output" \
+python3 "$repo/script/make_cite_input_mask.py" --data_dir "$par_output" \
   --hgnc_complete_set_path "$meta_resources_dir/hgnc_complete_set.txt" \
   --reactome_pathways_path "$meta_resources_dir/ReactomePathways.gmt"
 
 echo "Training multi"
-python3 scripts/train_mode.py --data_dir "$par_output" --task_type multi 
+python3 "$repo/scripts/train_mode.py" --data_dir "$par_output" --task_type multi 
 
 echo "Training cite"
-python3 scripts/train_mode.py --data_dir "$par_output" --task_type cite 
+python3 "$repo/scripts/train_mode.py" --data_dir "$par_output" --task_type cite 
